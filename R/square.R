7fd89fbc327df9a1ea97e58bd8ee36d91f5a552d
Gao Wang <gaow@uchicago.edu>
2015-07-23 19:11:44 +0000

    upload files

---
+++ R/square.R
@@ -0,0 +1,9 @@
+%11
+
+args <- strtoi(commandArgs(TRUE))
+
+square <- function(x) {
+  return(x * x)
+}
+
+print(paste0("Your answer is: ", toString(square(args[[1]]))))
---
+++ README.md
@@ -0,0 +1,5 @@
+# Smart Squaring
+
+Square numbers intelligently. Basically, $ x^2 = x * x $.
+
+(c) 2015, Gao and Mengyin.
